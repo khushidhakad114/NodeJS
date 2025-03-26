cfd513dae75a8b918987543abd0ad787b421df00
Khushi <khushidhakad2003@gmail.com>
2025-03-26 18:36:59 +0530

    middleware

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -58,6 +58,7 @@ const loginUser = async (req, res) => {
     res.status(200).json({
       message: "Login successful",
       user,
+      token,
     });
   } catch (err) {
     console.error(err);
